f7ce11d35fd9986e5de9f5418f222d43ced4ca68
jpzm <ignotus21@gmail.com>
2014-02-24 01:10:02 -0300

    Update the license file name

--- core/Info.py
+++ core/Info.py
@@ -26,5 +26,5 @@ INFO = {'name' :     'RadialNet',
         'version':   '0.44',
         'website':   'http://www.dca.ufrn.br/~joaomedeiros/radialnet/',
         'authors':   ['João Paulo de Souza Medeiros'],
-        'license':   file(os.path.join(path.get_dirbase(), 'COPYING')).read(),
+        'license':   file(os.path.join(path.get_dirbase(), 'LICENSE')).read(),
         'copyright': 'Copyright (C) 2007, 2008 Insecure.Com LLC'}
